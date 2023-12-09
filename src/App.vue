58cf50f48ab95e3c4a55ea5386f740988e4ab761
BeomSeokYu <beomseok.dev@gmail.com>
2023-12-09 12:03:13 +0900

    ~ checkbox binding

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>TD</h1>
+    <form
+      @submit.prevent="onSubmit">
+      <div class="d-flex">
+        <div class="flex-grow-1 mr-2">
+          <input
+            class="form-control"
+            type="text"
+            v-model="todo"
+            placeholder="Type new To do">
+        </div>
+        <div>
+          <button class="btn btn-primary">Add</button>
+        </div>
+      </div>
+      <div
+        class="error-message"
+        v-show="hasError">
+        This field cannot be empty.
+      </div>
+    </form>
+    <template v-for="item in todos" :key="item.id">
+      <div class="card mt-2">
+        <div class="card-body p-2">
+          <div class="form-check">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              v-model="item.complated">
+            <label class="form-check-label">{{ item.subject }}</label>
+          </div>
+        </div>
+      </div>
+    </template>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+
+    const todo = ref('');
+
+    const todos = ref([]);
+
+    const hasError = ref(false);
+
+    const onSubmit = () => {
+      if (todo.value === '') {
+        hasError.value = true;
+        return;
+      }
+
+      todos.value.push({
+        id: Date.now(),
+        subject: todo.value,
+        complated: false
+      })
+      todo.value = '';
+      hasError.value = false;
+    }
+
+    return {
+      todo,
+      todos,
+      hasError,
+      onSubmit
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.error-message {
+  color: red;
 }
 </style>
