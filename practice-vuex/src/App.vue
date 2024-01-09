8d1b47bf1034374395a6760dd26adef2a665a786
BeomSeokYu <beomseok.dev@gmail.com>
2024-01-09 10:49:34 +0900

    create vuex project

---
+++ practice-vuex/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <span>{{ count }}</span>
+    <button @click="increment">increment</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  },
+  computed: {
+    count() {
+      return this.$store.state.count
+    }
+  },
+  methods: {
+    increment() {
+      this.$store.commit('increment')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
